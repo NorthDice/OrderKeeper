2e9f35142046c44af9e5f4912e1ee6a9c9cff315
NorthDice <mister.balychev@gmail.com>
2025-06-19 20:43:09 +0300

    add: authorization access

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -1,5 +1,69 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
 
-func (h *Handler) userIdentity(c *gin.Context) {}
+const (
+	IsEmptyString = ""
+)
+const (
+	authorizationHeader = "Authorization"
+	userCtx             = "userId"
+)
+const (
+	InvalidHeader = "INVALID_HEADER"
+	InvalidToken  = "INVALID_TOKEN"
+	EmptyToken    = "EMPTY_TOKEN"
+)
+
+func (h *Handler) userIdentity(c *gin.Context) {
+	header := c.GetHeader(authorizationHeader)
+	if header == IsEmptyString {
+		c.JSON(http.StatusUnauthorized, ErrorResponse{
+			Error:   "token is missing",
+			Code:    EmptyToken,
+			Details: "token is missing",
+		})
+		return
+	}
+
+	headerParts := strings.Split(header, " ")
+	if len(headerParts) != 2 {
+		c.JSON(http.StatusUnauthorized, ErrorResponse{
+			Error:   "Invalid authorization header",
+			Code:    InvalidToken,
+			Details: "Invalid authorization header",
+		})
+		return
+	}
+
+	userId, err := h.services.Authorization.ParseToken(c.Request.Context(), headerParts[1])
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, ErrorResponse{
+			Error:   "Invalid authorization header",
+			Code:    InvalidHeader,
+			Details: err.Error(),
+		})
+		return
+	}
+
+	c.Set(userCtx, userId)
+}
+
+func getUserId(c *gin.Context) (int, error) {
+	id, ok := c.Get(userCtx)
+	if !ok {
+		return 0, fmt.Errorf("user id is not found")
+	}
+
+	idInt, ok := id.(int)
+	if !ok {
+		return 0, fmt.Errorf("user id is of invalid type")
+	}
+
+	return idInt, nil
+}
